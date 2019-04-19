15b86692b35a03bee99433a4c09ce7ec7c09f727
Brian May <brian@linuxpenguins.xyz>
2019-04-19 21:18:13 +1000

    Ensure db fields are not nullable

--- priv/repo/migrations/20190418082347_create_contacts.exs
+++ priv/repo/migrations/20190418082347_create_contacts.exs
@@ -3,9 +3,9 @@ defmodule PhoneDb.Repo.Migrations.CreateContacts do
 
   def change do
     create table(:contacts) do
-      add :phone_number, :string
-      add :name, :string
-      add :action, :string
+      add :phone_number, :string, null: false
+      add :name, :string, null: false
+      add :action, :string, null: false
 
       timestamps()
     end
