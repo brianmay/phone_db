0ee485d5d143639db86d62a5c4d70ec99a3f1a73
Brian May <brian@linuxpenguins.xyz>
2019-04-21 10:56:58 +1000

    Customize front page and header

--- test/phone_db_web/controllers/page_controller_test.exs
+++ test/phone_db_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PhoneDbWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Phone DB!"
   end
 end
