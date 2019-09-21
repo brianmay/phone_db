27c397a8c6a2587f3eefa7e5795392f797c1a03c
Brian May <brian@linuxpenguins.xyz>
2019-09-21 13:25:34 +1000

    Add required polyfill

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,7 @@ import "phoenix_html"
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 
+import "mdn-polyfills/Array.from"
 import "mdn-polyfills/NodeList.prototype.forEach"
 import "mdn-polyfills/Element.prototype.closest"
 import "mdn-polyfills/Element.prototype.matches"
