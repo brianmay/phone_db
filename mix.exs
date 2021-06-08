900d2ac54768c91d99698dfeaa930076f66f7da4
Brian May <brian@linuxpenguins.xyz>
2021-06-08 13:26:52 +1000

    Synchronise with other projects

--- mix.exs
+++ mix.exs
@@ -65,7 +65,8 @@ defmodule PhoneDb.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"],
+      prettier: "cmd ./assets/node_modules/.bin/prettier --check . --color"
     ]
   end
 
