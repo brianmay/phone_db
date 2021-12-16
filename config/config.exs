4be2b367475d6a4ec49da3053438045583413968
Brian May <brian@linuxpenguins.xyz>
2021-12-16 13:34:12 +1100

    Upgrade elixir/erlang

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 # is restricted to this project.
 
 # General application configuration
-use Mix.Config
+import Config
 
 config :phone_db,
   ecto_repos: [PhoneDb.Repo],
--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 config :phone_db,
   sync_services: [],
