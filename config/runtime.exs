538c3e5f37ddef33391cd7c4b087720287ff0cfe
Brian May <brian@linuxpenguins.xyz>
2023-10-21 10:33:32 +1100

    Fix runtime config error

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,7 +17,7 @@ config :phone_db, PhoneDb.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 port = String.to_integer(System.get_env("PORT") || "4000")
-http_url = System.get_env("HTTP_URL")
+http_url = System.get_env("HTTP_URL") || "http://localhost:#{port}"
 http_uri = URI.parse(http_url)
 
 config :phone_db, PhoneDbWeb.Endpoint,
