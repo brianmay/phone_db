bf7198bc976ea020f5f9ea90bc9609b138046d38
Brian May <brian@linuxpenguins.xyz>
2024-09-23 07:42:31 +1000

    fix: reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -2,165 +2,169 @@
   description = "Phone Database";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-24.05"; };
-    flake-utils = { url = "github:numtide/flake-utils"; };
-    devenv = { url = "github:cachix/devenv"; };
+    nixpkgs = {url = "github:NixOS/nixpkgs/nixos-24.05";};
+    flake-utils = {url = "github:numtide/flake-utils";};
+    devenv = {url = "github:cachix/devenv";};
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, devenv }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        inherit (pkgs.lib) optional optionals;
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    devenv,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      inherit (pkgs.lib) optional optionals;
+      pkgs = nixpkgs.legacyPackages.${system};
 
-        elixir = pkgs.beam.packages.erlang.elixir;
-        beamPackages = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang;
+      elixir = pkgs.beam.packages.erlang.elixir;
+      beamPackages = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang;
 
-        src = ./.;
-        version = "0.0.0";
-        pname = "phone_db";
+      src = ./.;
+      version = "0.0.0";
+      pname = "phone_db";
 
-        mixFodDeps = beamPackages.fetchMixDeps {
-          TOP_SRC = src;
-          pname = "${pname}-mix-deps";
-          inherit src version;
-          hash = "sha256-gLe1JR29hyIIRRyLF6rfoagL02YJfXMKfnkRXF5R1gY=";
-          # hash = pkgs.lib.fakeHash;
-        };
+      mixFodDeps = beamPackages.fetchMixDeps {
+        TOP_SRC = src;
+        pname = "${pname}-mix-deps";
+        inherit src version;
+        hash = "sha256-gLe1JR29hyIIRRyLF6rfoagL02YJfXMKfnkRXF5R1gY=";
+        # hash = pkgs.lib.fakeHash;
+      };
 
-        nodejs = pkgs.nodejs;
-        nodePackages =
-          import assets/default.nix { inherit pkgs system nodejs; };
+      nodejs = pkgs.nodejs;
+      nodePackages =
+        import assets/default.nix {inherit pkgs system nodejs;};
 
-        pkg = beamPackages.mixRelease {
-          TOP_SRC = src;
-          inherit pname version elixir src mixFodDeps;
+      pkg = beamPackages.mixRelease {
+        TOP_SRC = src;
+        inherit pname version elixir src mixFodDeps;
 
-          postBuild = ''
-            ln -sf ${mixFodDeps}/deps deps
-            ln -sf ${nodePackages.nodeDependencies}/lib/node_modules assets/node_modules
-            export PATH="${nodePackages.nodeDependencies}/bin:$PATH"
-            ${nodejs}/bin/npm run deploy --prefix ./assets
+        postBuild = ''
+          ln -sf ${mixFodDeps}/deps deps
+          ln -sf ${nodePackages.nodeDependencies}/lib/node_modules assets/node_modules
+          export PATH="${nodePackages.nodeDependencies}/bin:$PATH"
+          ${nodejs}/bin/npm run deploy --prefix ./assets
 
-            # for external task you need a workaround for the no deps check flag
-            # https://github.com/phoenixframework/phoenix/issues/2690
-            mix do deps.loadpaths --no-deps-check, phx.digest
-            mix phx.digest --no-deps-check
-          '';
-
-        };
-
-        psql = pkgs.writeShellScriptBin "pd_psql" ''
-          exec "${pkgs.postgresql}/bin/psql" "$DATABASE_URL" "$@"
+          # for external task you need a workaround for the no deps check flag
+          # https://github.com/phoenixframework/phoenix/issues/2690
+          mix do deps.loadpaths --no-deps-check, phx.digest
+          mix phx.digest --no-deps-check
         '';
-        port = 4000;
-        postgres_port = 6101;
-        ldap_port = 6102;
+      };
 
-        ldap_dir = ".devenv/state/ldap";
-        ldap_url = "ldap://localhost:${toString ldap_port}/";
+      psql = pkgs.writeShellScriptBin "pd_psql" ''
+        exec "${pkgs.postgresql}/bin/psql" "$DATABASE_URL" "$@"
+      '';
+      port = 4000;
+      postgres_port = 6101;
+      ldap_port = 6102;
 
-        phone_username = "phone";
-        phone_password = "password";
+      ldap_dir = ".devenv/state/ldap";
+      ldap_url = "ldap://localhost:${toString ldap_port}/";
 
-        ldap_schemas = pkgs.stdenv.mkDerivation {
-          name = "ldap_schemas";
-          src = ./ldap_schemas;
-          installPhase = ''
-            mkdir $out
-            cp -a * $out
-          '';
-        };
-        dn_suffix = "dc=python-ldap,dc=org";
-        root_dn = "cn=root,${dn_suffix}";
+      phone_username = "phone";
+      phone_password = "password";
 
-        slapd_config = pkgs.writeTextFile {
-          name = "slapd.conf";
-          text = ''
-            include ${ldap_schemas}/00-core.schema
-            include ${ldap_schemas}/10-cosine.schema
-            include ${ldap_schemas}/50-inetorgperson.schema
-            include ${ldap_schemas}/70-eduperson.schema
-            include ${ldap_schemas}/90-aueduperson.schema
-            include ${ldap_schemas}/90-schac.schema
-            include ${ldap_schemas}/nis.schema
-            allow bind_v2
-
-            # Database
-            moduleload back_mdb
-            moduleload ppolicy
-
-            database mdb
-            directory ${ldap_dir}
-
-            suffix ${dn_suffix}
-            overlay ppolicy
-            ppolicy_default cn=default,${dn_suffix}
-
-            access to dn.sub=${dn_suffix} attrs=userPassword
-               by anonymous auth
-
-            access to dn.sub=${dn_suffix}
-               by dn.exact=${root_dn} write
-          '';
-        };
-
-        admin_ldif = pkgs.writeTextFile {
-          name = "admin.ldif";
-          text = ''
-            dn: ${dn_suffix}
-            o: Test Org
-            objectClass: dcObject
-            objectClass: organization
-
-            dn: ${root_dn}
-            cn: ${root_dn}
-            objectClass: simpleSecurityObject
-            objectClass: organizationalRole
-            userPassword: your_secure_password_here
-
-            dn: cn=default,${dn_suffix}
-            objectClass: top
-            objectClass: device
-            objectClass: pwdPolicy
-            pwdAttribute: userPassword
-            pwdLockout: TRUE
-
-            dn: ou=People,${dn_suffix}
-            objectClass: top
-            objectClass: OrganizationalUnit
-            ou: People
-
-            dn: ou=Groups,${dn_suffix}
-            objectClass: top
-            objectClass: OrganizationalUnit
-          '';
-        };
-
-        pd_ldapsearch = pkgs.writeShellScriptBin "pd_ldapsearch" ''
-          exec ldapsearch -H "${ldap_url}" -D "${root_dn}" -b "${dn_suffix}" -w your_secure_password_here
+      ldap_schemas = pkgs.stdenv.mkDerivation {
+        name = "ldap_schemas";
+        src = ./ldap_schemas;
+        installPhase = ''
+          mkdir $out
+          cp -a * $out
         '';
+      };
+      dn_suffix = "dc=python-ldap,dc=org";
+      root_dn = "cn=root,${dn_suffix}";
 
-        start_ldap = pkgs.writeShellScriptBin "start_ldap" ''
-          set -e
-          set -x
-          if ! test -d "${ldap_dir}"; then
-            mkdir "${ldap_dir}"
-            cat "${admin_ldif}"
-            "${pkgs.openldap}/bin/slapadd" -n 1 -f "${slapd_config}" -l "${admin_ldif}"
-          fi
-          "${pkgs.openldap}/libexec/slapd" -f "${slapd_config}" -h "${ldap_url}"  -d 1
+      slapd_config = pkgs.writeTextFile {
+        name = "slapd.conf";
+        text = ''
+          include ${ldap_schemas}/00-core.schema
+          include ${ldap_schemas}/10-cosine.schema
+          include ${ldap_schemas}/50-inetorgperson.schema
+          include ${ldap_schemas}/70-eduperson.schema
+          include ${ldap_schemas}/90-aueduperson.schema
+          include ${ldap_schemas}/90-schac.schema
+          include ${ldap_schemas}/nis.schema
+          allow bind_v2
+
+          # Database
+          moduleload back_mdb
+          moduleload ppolicy
+
+          database mdb
+          directory ${ldap_dir}
+
+          suffix ${dn_suffix}
+          overlay ppolicy
+          ppolicy_default cn=default,${dn_suffix}
+
+          access to dn.sub=${dn_suffix} attrs=userPassword
+             by anonymous auth
+
+          access to dn.sub=${dn_suffix}
+             by dn.exact=${root_dn} write
         '';
+      };
 
-        test_phone_call = pkgs.writeShellScriptBin "test_phone_call" ''
-          curl --json '{"phone_number":"1", "destination_number":"2"}' --user "${phone_username}:${phone_password}" "http://localhost:${
-            toString port
-          }/api/incoming_call/"
+      admin_ldif = pkgs.writeTextFile {
+        name = "admin.ldif";
+        text = ''
+          dn: ${dn_suffix}
+          o: Test Org
+          objectClass: dcObject
+          objectClass: organization
+
+          dn: ${root_dn}
+          cn: ${root_dn}
+          objectClass: simpleSecurityObject
+          objectClass: organizationalRole
+          userPassword: your_secure_password_here
+
+          dn: cn=default,${dn_suffix}
+          objectClass: top
+          objectClass: device
+          objectClass: pwdPolicy
+          pwdAttribute: userPassword
+          pwdLockout: TRUE
+
+          dn: ou=People,${dn_suffix}
+          objectClass: top
+          objectClass: OrganizationalUnit
+          ou: People
+
+          dn: ou=Groups,${dn_suffix}
+          objectClass: top
+          objectClass: OrganizationalUnit
         '';
+      };
 
-        devShell = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [{
+      pd_ldapsearch = pkgs.writeShellScriptBin "pd_ldapsearch" ''
+        exec ldapsearch -H "${ldap_url}" -D "${root_dn}" -b "${dn_suffix}" -w your_secure_password_here
+      '';
+
+      start_ldap = pkgs.writeShellScriptBin "start_ldap" ''
+        set -e
+        set -x
+        if ! test -d "${ldap_dir}"; then
+          mkdir "${ldap_dir}"
+          cat "${admin_ldif}"
+          "${pkgs.openldap}/bin/slapadd" -n 1 -f "${slapd_config}" -l "${admin_ldif}"
+        fi
+        "${pkgs.openldap}/libexec/slapd" -f "${slapd_config}" -h "${ldap_url}"  -d 1
+      '';
+
+      test_phone_call = pkgs.writeShellScriptBin "test_phone_call" ''
+        curl --json '{"phone_number":"1", "destination_number":"2"}' --user "${phone_username}:${phone_password}" "http://localhost:${
+          toString port
+        }/api/incoming_call/"
+      '';
+
+      devShell = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
             enterShell = ''
               export HTTP_URL="http://localhost:${toString port}"
               export PORT="${toString port}"
@@ -195,7 +199,8 @@
                 glibcLocales
                 node2nix
                 nodejs
-              ] ++ optional stdenv.isLinux inotify-tools
+              ]
+              ++ optional stdenv.isLinux inotify-tools
               ++ optional stdenv.isDarwin terminal-notifier
               ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
                 CoreFoundation
@@ -216,10 +221,10 @@
             };
             services.postgres = {
               enable = true;
-              package = pkgs.postgresql_15.withPackages (ps: [ ps.postgis ]);
+              package = pkgs.postgresql_15.withPackages (ps: [ps.postgis]);
               listen_addresses = "127.0.0.1";
               port = postgres_port;
-              initialDatabases = [{ name = "phone_db"; }];
+              initialDatabases = [{name = "phone_db";}];
               initialScript = ''
                 \c phone_db;
                 CREATE USER phone_db with encrypted password 'your_secure_password_here';
@@ -227,15 +232,17 @@
                 ALTER USER phone_db WITH SUPERUSER;
               '';
             };
-          }];
-        };
-      in {
-        packages = {
-          devenv-up = devShell.config.procfileScript;
-          default = pkg;
-        };
-        inherit devShell;
-      }) // {
-        nixosModules.default = import ./module.nix { inherit self; };
+          }
+        ];
       };
+    in {
+      packages = {
+        devenv-up = devShell.config.procfileScript;
+        default = pkg;
+      };
+      inherit devShell;
+    })
+    // {
+      nixosModules.default = import ./module.nix {inherit self;};
+    };
 }
